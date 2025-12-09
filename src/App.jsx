e8b3e5653812db78887f7f4595fd63ed978e9821
Pomelodies <randomelodies@gmail.com>
2025-12-09 15:33:25 +0100

    list of game displayed & search bar active

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <>
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home search={search} setSearch={setSearch} />}
+          />
         </Routes>
       </Router>
     </>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,39 @@
 import "./home.css";
 import { TbDeviceGamepad3Filled } from "react-icons/tb";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, Links } from "react-router-dom";
 
-import "./home.css";
+const Home = ({ search, setSearch }) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-const Home = () => {
-  return (
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        let filters = "";
+        if (search) {
+          filters = filters + `&search="${search}"`;
+        }
+        console.log(filters);
+        const response = await axios.get(
+          `https://api.rawg.io/api/games?key=f60dfb57a6af4a60b940f680f44697bb&${filters}&search_precise="true"`
+        );
+        // console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      };
+      fetchData();
+    } catch (error) {
+      console.log("error => ", error);
+    }
+  }, [search]);
+
+  return isLoading ? (
+    <main>
+      <p>Data is loading, please wait...!</p>
+    </main>
+  ) : (
     <main>
       <div className="container">
         <div className="logo">
@@ -12,8 +41,32 @@ const Home = () => {
           <h1>Gamepad</h1>
         </div>
         <div className="search">
-          <input type="text" placeholder="Search for a game..." />
-          <p>Search xx games</p>
+          <input
+            type="text"
+            placeholder="Search for a game..."
+            value={search}
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+          <p>Search {data.count.toLocaleString()} games</p>
+        </div>
+        <h2>Most Relevance Games</h2>
+        <div className="all-games">
+          {data.results.map((game) => {
+            // console.log(game);
+            return (
+              <Link key={game.id} className="link-article">
+                <article>
+                  <p>{game.name}</p>
+                  <img
+                    src={game.background_image}
+                    alt={`cover of ${game.name}`}
+                  />
+                </article>
+              </Link>
+            );
+          })}
         </div>
       </div>
     </main>
