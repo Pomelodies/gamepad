40b3ce511b4630b2b0ea2afb6f72b1f00e5d6c1a
Pomelodies <randomelodies@gmail.com>
2025-12-18 16:18:10 +0100

    signup and login pages added

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -3,8 +3,16 @@ import { CgProfile } from "react-icons/cg";
 import { TbPlaylistAdd } from "react-icons/tb";
 import { MdOutlineRateReview } from "react-icons/md";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
   return (
     <main>
       <div className="container">
@@ -29,12 +37,69 @@ const Signup = () => {
           </section>
           <section className="input-signup">
             <h2>Sign Up</h2>
-            <form action="">
-              <input type="text" placeholder="Username" />
-              <input type="email" placeholder="Email" />
-              <input type="password" placeholder="Password" />
-              <input type="password" placeholder="Confirm Password" />
-              <input type="file" />
+            <form
+              onSubmit={async (event) => {
+                event.preventDefault();
+                try {
+                  const response = await axios.post(
+                    "http://localhost:3000/signup",
+                    {
+                      username: username,
+                      email: email,
+                      password: password,
+                    }
+                  );
+                  // console.log(response);
+                  
+                } catch (error) {
+                  error.response
+                    ? setErrorMessage(error.response.data.message)
+                    : console.log(error);
+                }
+              }}
+            >
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+              />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+                value={email}
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
+              />
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(event) => {
+                  setPassword(event.target.value);
+                }}
+              />
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                placeholder="Confirm Password"
+                value={confirmPassword}
+                onChange={(event) => {
+                  setConfirmPassword(event.target.value);
+                }}
+              />
+              {/* <input type="file" /> */}
+              {errorMessage && <p className="error">{errorMessage}</p>}
               <button>Sign Up</button>
             </form>
             <Link className="linkToLogin" to="/login">
